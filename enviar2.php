8822bdb35dd7920fdb1e0da3b8a5b248c28da9a0
Frank Aveiga <xfpad93@gmail.com>
2021-01-04 17:19:24 -0500

    pagina terminada sin alianzas

---
+++ enviar2.php
@@ -0,0 +1,27 @@
+<?php
+$name = $_POST['name'];
+$mail = $_POST['mail'];
+
+$message = $_POST['message'];
+$header = 'From: ' . $name . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$message = "El motivo de este mensaje es: " . $name . " \r\n";
+$message .= "Telefono: " . $_POST['phone'] . " \r\n";
+$message .= "E-mail: " . $_POST['mail'] . " \r\n";
+$message .= "Ubicacion: " . $_POST['ubicacion'] . " \r\n";
+$message .= "Hectareas: " . $_POST['hectareas'] . " \r\n";
+$message .= "Tipo de cultivo: " . $_POST['tipo'] . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+
+$message .= "Enviado el: " . date('d/m/Y', time());
+
+$para = 'xfpad93@gmail.com';
+$asunto = 'Desde la pagina web';
+
+mail($para, $asunto, utf8_decode($message), $header);
+
+header("Location:enviado.html");
+?>
